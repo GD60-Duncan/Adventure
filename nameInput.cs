741c44eb0365d853377ac13bb3b5281f92e205f7
Duncan McDougall <DuncanrMcDougall@icloud.com>
2020-10-07 12:47:49 -0400

    thank god

---
+++ nameInput.cs
@@ -0,0 +1,24 @@
+//Copyright (C) 2020 Duncan McDougall, All rights Reserved
+using System;
+
+namespace Adventure
+{
+    public class nameInput
+    {
+        
+        
+        //public string userName = Console.ReadLine();
+        
+        //public string nameInput;
+        public static void Start()
+        {
+            Console.WriteLine("Welcome to the city, What is your name?");
+            Console.WriteLine(" ");
+            Console.WriteLine("Input your answer here");
+            
+
+            return;
+            
+        }
+    }
+}
